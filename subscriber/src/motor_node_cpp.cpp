7e330a46e8342f82fecc06efa0790a9bb001755c
Hayden11211 <haydenranderson@vt.edu>
2025-10-14 18:02:45 -0400

    Finished

--- subscriber/src/motor_node_cpp.cpp
+++ subscriber/src/motor_node_cpp.cpp
@@ -36,19 +36,20 @@ class MotorNode : public rclcpp::Node
     {
       //this is where you assign subscribers and publishers to a topic.
 
-      velocity_subscriber = this->create_subscription</* geometry_msgs::msg::Twist */>( // TODO(1): fill in message type for your velocity subscriber! should be a twist message type.
-      "/TODO(2)", 10, std::bind(&MotorNode::your_callback, this, _1)); //TODO(2), change topic so subscriber listens to /cmd_vel topic. You should change the string on this line
+      velocity_subscriber = this->create_subscription<geometry_msgs::msg::Twist>( // TODO(1): fill in message type for your velocity subscriber! should be a twist message type.
+      "/cmd_vel", 10, std::bind(&MotorNode::your_callback, this, _1)); //TODO(2), change topic so subscriber listens to /cmd_vel topic. You should change the string on this line
 
 
 
 
       left_publisher = this->create_publisher<std_msgs::msg::Float64>("/left_drive", 10);//example for left publisher.
+      right_publisher = this->create_publisher<std_msgs::msg::Float64>("/right_drive", 10);
       //TODO(3) - assign the right publisher you create to a topic 
     }
 
   private:
     void your_callback(geometry_msgs::msg::Twist::SharedPtr msg)
-    {
+    {  
       /***
        * TODO(4)
        * the method "your_callback" is run everytime your subscriber recieves a message. Here, do the math to find
@@ -58,15 +59,23 @@ class MotorNode : public rclcpp::Node
 
 
 
-        double lin_x = msg->linear.x; // this is how you get the x velocity from the message. Try angular z!
+        double lin_x_L = msg->linear.x;
+        double ang_z_L = msg->angular.z; // this is how you get the x velocity from the message. Try angular z!
 
         std_msgs::msg::Float64 left_velocity;
 
-        left_velocity.data = //ROS2 messages require you to use .data to add information to the variable you are publishing
-        left_publisher->publish(left_velocity) 
+        left_velocity.data = lin_x_L  - 0.5 * ang_z_L;//ROS2 messages require you to use .data to add information to the variable you are publishing
+        left_publisher->publish(left_velocity); 
         /*** this is how you would publish a variable.
         Note that the variable is incomplete at this point (you have to do the math at the top to assign left_velocity to the right value)
         */
+       double lin_x_R = msg->linear.x;
+       double ang_z_R = msg->angular.z;
+
+       std_msgs::msg::Float64 right_velocity;
+
+       right_velocity.data = lin_x_R + 0.5 * ang_z_L;
+       right_publisher->publish(right_velocity);
     }
 
 
@@ -75,7 +84,8 @@ class MotorNode : public rclcpp::Node
 
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr velocity_subscriber; //note the message type (hint - TODO(1))
 
-    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr left_publisher; // example for the /left_drive publisher. You need to make the right publisher.
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr left_publisher;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr right_publisher; // example for the /left_drive publisher. You need to make the right publisher.
     //TODO(5) - create the publisher for the right motor
     
 };
